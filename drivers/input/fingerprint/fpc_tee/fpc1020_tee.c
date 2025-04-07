022b97cc72eefef2a0afef7091914d8d8f471c1c
onettboots <blackcocopet@gmail.com>
2025-04-07 13:24:02 +0000

    Revert "drivers: fingerprint: fpc_tee: Don't require a dts node to set IRQF_NO_SUSPEND"
    This reverts commit 0ae67bee42cdb2810e3c84bd3300182d5fa925bd.

--- drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
@@ -682,9 +682,12 @@ static int fpc1020_probe(struct platform_device *pdev)
 
 	atomic_set(&fpc1020->wakeup_enabled, 0);
 
-	fpc1020->irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT | IRQF_NO_SUSPEND;
+	fpc1020->irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT;
 	fpc1020->gpios_requested = false;
-	device_init_wakeup(dev, 1);
+	if (of_property_read_bool(dev->of_node, "fpc,enable-wakeup")) {
+		fpc1020->irqf |= IRQF_NO_SUSPEND;
+		device_init_wakeup(dev, 1);
+	}
 
 	mutex_init(&fpc1020->lock);
 	fpc1020->ttw_ws = wakeup_source_register(dev, "fpc_ttw_ws");
